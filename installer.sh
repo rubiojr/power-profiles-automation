146536c56991bc28048d7c2045bed327de0f0860
Sergio Rubio <rubiojr@rbel.co>
2024-03-25 12:26:12 +0100

    Cosmetic changes, no need to reboot

--- installer.sh
+++ installer.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+INSTALL_PATH="/usr/local/power-profiles"
 # Script to install power profile automation based on the presence of AC power
+if [ $EUID -ne 0 ]; then
+  exec sudo $0
+else
+  echo "This script will be run as sudo..."
+fi
 
-echo "This script will be run as sudo."
-echo "Creating /usr/share/power-profiles"
-sudo mkdir /usr/share/power-profiles
-echo "Copying the scripts to /usr/share/power-profiles"
-sudo cp usr_share_power-profiles/* /usr/share/power-profiles
+echo "Creating $INSTALL_PATH"
+sudo mkdir -p "$INSTALL_PATH"
+echo "Copying the scripts to $INSTALL_PATH"
+sudo cp usr_share_power-profiles/* "$INSTALL_PATH"
 echo "Copying udev rules to /etc/udev/rules.d"
 sudo cp etc_udev_rules.d/* /etc/udev/rules.d
-echo "Done. Reboot to start automation."
+echo "Reloading udev rules"
+sudo udevadm control --reload-rules && sudo udevadm trigger
 
